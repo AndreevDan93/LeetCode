5775eaa7c45a644989ad0bdcf5e72a09355ee5c5
Даниил Андреев <104764384+AndreevDan93@users.noreply.github.com>
2023-06-19 13:27:25 +0000

    Sync LeetCode submission - Min Stack (java)

---
+++ problems/min_stack/solution.java
@@ -0,0 +1,46 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int val) {
+       stack.push(val);
+       if (min.empty()) {
+           min.push(val);
+       } else if (min.peek() >= val) {
+           min.push(val);
+       }
+
+
+
+    }
+
+    public void pop() {
+        int popped = stack.pop();
+        if (!min.empty() && min.peek() == popped) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
